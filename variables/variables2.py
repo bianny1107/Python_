08e925600ad83d4df165c2f92368f0e6274a4367
Bianny Peña <bianny1107@gmail.com>
2025-02-25 17:30:19 -0400

    variables2 updated

---
+++ variables/variables2.py
@@ -0,0 +1,12 @@
+# CONCATENACIÓN: Se utiliza el signo +.
+
+x = 'Bianny'
+y = 'Michelle'
+print(x + ' ' + y) #Se dejan las comillas con el espacio para que las cadenas no se impriman juntas.
+
+x = 1
+y = 2
+print(x + y) #Suma los valores porque ambos son enteros.
+print(3 + 5)
+
+#OJO: NO se deben concatenar tipos de datos diferentes, sino da error.
